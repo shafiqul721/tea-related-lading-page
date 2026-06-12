43d014fbfa296842c6ad5773dc7ea50ce0acbc4e
Mohammad Shuvo <mohammadshuvo721@gmail.com>
2026-06-12 23:14:50 +0800

    added the inital configatation lke ,fonts,tailwind,icons

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// for tailwind autosuggestion while codeing
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
